2a486fb1b1076424fa2f6cb92c60c607b7afd54a
Brahim <amghough551@gmail.com>
2024-05-16 00:09:21 +0100

    add - docker installation script

--- p3/bamghoug-p3/scripts/bootstrap.sh
+++ p3/bamghoug-p3/scripts/bootstrap.sh
@@ -5,6 +5,28 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 # copy the installed kubectl file to /usr/local/bin/kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
+# install docker
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
 # get k3D install script and pipe it to bash
 sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
@@ -14,7 +36,7 @@ sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
 
 # Create cluster
-sudo k3d cluster create -p "80:80"
+sudo k3d cluster create
 
 # create argocd namespace
 sudo kubectl apply -f confs/namespaces/argocd-namespace.yaml
@@ -45,4 +67,4 @@ sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null &
 sudo argocd admin initial-password -n argocd
 
 # 
-sudo argocd app create bamghoug-iot-p3 --repo https://github.com/amg-1337bg/bamghoug-IOT-p3 --path bamghoug-iot-p3 --dest-server https://kubernetes.default.svc --dest-namespace dev
+sudo argocd app create bamghoug-iot-p3 --repo https://github.com/amg-1337bg/bamghoug-IOT-p3 --path bamghoug-IOT-p3 --dest-server https://kubernetes.default.svc --dest-namespace default --sync-policy auto
